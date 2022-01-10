b7d2b6acff1c9a2b0655785b5003e882da217af5
Amari L <70532194+S11013885@users.noreply.github.com>
2022-01-10 09:18:30 -0500

    v0.1
    added variables for the cereal name cups and fiber

--- DataStudentCode/Cereal.java
+++ DataStudentCode/Cereal.java
@@ -1,1 +1,15 @@
-// Amari Lombel, 1/10/22, AP Data Labs 5B
+// Amari Lombel, 1/10/22, AP Data Labs 2022 v0.1
+
+public class cereal
+{
+
+    private String cerealName;
+    private double cups;
+    private double fiber;
+    private double fiberPerCups;
+
+
+
+
+
+}
